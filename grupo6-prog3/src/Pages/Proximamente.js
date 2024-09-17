6b30e1f6238ad8978b16aa8f006bb3d2ec0811a0
Juan Tutaglio <JuanTutaglio@MacBook-Pro-de-Mauge.local>
2024-09-17 17:12:05 -0300

    favoritos y navegacion

---
+++ grupo6-prog3/src/Pages/Proximamente.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Proximamente extends Component{
+    constructor(props){
+        super(props);
+    }
+    
+    render() {
+        return(
+            <>
+                <p>Proximamente</p>
+            </>
+        )
+    }
+}
+
+export default Proximamente
